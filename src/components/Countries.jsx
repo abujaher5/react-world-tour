86f6a2606636dfd1fc29e6c4d4239e10d61e5af5
abujaher5 <abujaher5120@gmail.com>
2024-03-15 14:46:09 +0600

    complete vitited country name and flag

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,9 +1,14 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "./Country";
+import "./countries.css";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
+  const [visitedFlags, setVisitedFlags] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -11,12 +16,51 @@ const Countries = () => {
       .then((data) => setCountries(data));
   }, []);
 
+  const handleVisitedCountry = (country) => {
+    console.log("add this to your visited country");
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
+  const handleVisitedFlags = (flag) => {
+    const newVisitedFlags = [...visitedFlags, flag];
+    setVisitedFlags(newVisitedFlags);
+  };
+
+  // remove item from an array in a state
+  // use filter to select all the elements except the one you want to remove
+
   return (
     <div>
       <h3>Countries:{countries.length}</h3>
-      {countries.map((country) => (
-        <Country key={country.cca3} country={country}></Country>
-      ))}
+
+      {/* visited country */}
+      <div>
+        <h3>Visited Countries : {visitedCountries.length}</h3>
+        <ul>
+          {visitedCountries.map((country) => (
+            <li key={country.cca3}>{country.name.common}</li>
+          ))}
+        </ul>
+      </div>
+      {/* display visited flag */}
+
+      <div className="flag-container">
+        {visitedFlags.map((flag, idx) => (
+          <img key={idx} src={flag}></img>
+        ))}
+      </div>
+      {/* display countries */}
+
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country
+            key={country.cca3}
+            handleVisitedCountry={handleVisitedCountry}
+            handleVisitedFlags={handleVisitedFlags}
+            country={country}
+          ></Country>
+        ))}
+      </div>
     </div>
   );
 };
